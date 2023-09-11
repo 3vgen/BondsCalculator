f33b6fa1402901dfeedd8f7c586650db1ef4b4cd
Evgeniy Semin <evgeniy.zhenia@gmail.com>
2023-09-11 15:01:03 +0300

    adding bond if it already exists

--- main.py
+++ main.py
@@ -5,29 +5,24 @@ from config import *
 
 def add_bond(ticker, count_of_bonds, cur, conn) -> None:
     bd = BondModel(ticker)
-    query_for_calendar = f"INSERT INTO frequency_of_payment (bond_tickers, {', '.join(bd.get_coupon_frequency())})" \
-                         f" VALUES (\'{bd.sec_id}\'{len(bd.get_coupon_frequency()) * ', TRUE'});"
-
     query_for_main_info = "INSERT INTO bonds_information VALUES (%s, %s, %s, %s, %s, %s)"
 
     try:
-        cur.execute(query_for_main_info,
-                    (bd.sec_id, bd.name, bd.current_price, bd.coupon_value, count_of_bonds, bd.coupon_frequency))
-        print(query_for_main_info)
-
-        for i in bd.get_coupon_frequency():
-            print(
-                f"INSERT INTO frequency_of_payment2 (bonds_ticker, month_of_payment) VALUES (\'{bd.sec_id}\',\'{i}\')")
-            cur.execute(
-                f"INSERT INTO frequency_of_payment2 (bonds_ticker, month_of_payment) VALUES (\'{bd.sec_id}\',\'{i}\')")
+        cur.execute(f"select * from bonds_information where ticker = \'{bd.sec_id}\'")
+        rows = cur.fetchall()
+        if len(rows) != 0:
+            cur.execute(f"update bonds_information set bonds_count = bonds_count + {count_of_bonds} where ticker = \'{bd.sec_id}\'")
+        else:
+            cur.execute(query_for_main_info,
+                        (bd.sec_id, bd.name, bd.current_price, bd.coupon_value, count_of_bonds, bd.coupon_frequency))
+            for i in bd.get_coupon_frequency():
+                cur.execute(
+                    f"INSERT INTO frequency_of_payment2 (bonds_ticker, month_of_payment) VALUES (\'{bd.sec_id}\',\'{i}\')")
 
         conn.commit()
 
     except Exception as err:
         print("[INFO] database error", err)
-    finally:
-        cur.close()
-        conn.close()
 
 
 conn = psycopg2.connect(
@@ -38,7 +33,15 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-add_bond('RU000A106EZ0', 2, cur, conn)
+add_bond('RU000A106EZ0', 4, cur, conn)
+
+
+
+
+
+
+cur.close()
+conn.close()
 
 
 
